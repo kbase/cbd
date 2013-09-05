6216be2b516fc2397ec3f4db1de41b468adacbba
Mike Mundy <mundy.michael@mayo.edu>
2013-09-05 09:57:27 -0500

    Add examples to cbd-buildmatrix help text

--- scripts/cbd-buildmatrix.py
+++ scripts/cbd-buildmatrix.py
@@ -19,21 +19,21 @@ DESCRIPTION
       comparisons.  Compression based distance uses the relative compression
       of combined and individual datasets to quantify overlaps between
       microbial communities.
-      
+
       The input-path positional argument is the path to a file with the list of
       paths to the input sequence files.  Each line of of the list file is the
       path to a sequence file.  Each sequence file contains the sequence reads
       for a microbial community.  The --format optional argument specifies the
       type of the sequence files.  Valid formats include 'fasta', 'fastq',
       'clustal', 'embl', 'genbank', 'nexus, and 'seqxml'.
-      
+
       The output-path positional argument is the path to the output file where
       the distance matrix is stored.  The output file is in csv format with a
       row and column for each sequence file.  The value of each cell in the
       table is the distance between two microbial communities.  A value of 0
       means the two communities are identical and a value of 1 means the two
       communities are completely different.
-      
+
       The --scale optional argument specifies the scale of the distance values.
       A value of 'std' means to use the standard scale of 0 to 1.  A value of
       'inf' means to use a scale from 0 to infinity.
@@ -41,9 +41,15 @@ DESCRIPTION
 
 desc3 = '''
 EXAMPLES
+      Build a distance matrix for a set of fasta sequence files:
+      > cbd-buildmatrix mystudy.input mystudy.csv
+
+      Build a distance matrix for a set of fastq sequence files:
+      > cbd-buildmatrix --format fastq mystudy.input mystudy.csv
 
 AUTHORS
-      Fang Yang, Nicholas Chia, Bryan A White, Lawrence B Schook, Mike Mundy
+      Fang Yang, Nicholas Chia, Bryan A White, Lawrence B Schook, Patricio
+      Jeraldo, Mike Mundy
 '''
 
 if __name__ == "__main__":
