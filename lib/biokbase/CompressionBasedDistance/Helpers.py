e5e855dc2b799e99aa54c5d1fedd427b45f70f6d
Mike Mundy <mundy.michael@mayo.edu>
2013-10-14 16:09:54 -0500

    fix using cbd-url from an IRIS terminal

--- lib/biokbase/CompressionBasedDistance/Helpers.py
+++ lib/biokbase/CompressionBasedDistance/Helpers.py
@@ -7,58 +7,44 @@ from biokbase.CompressionBasedDistance.Shock import Client as ShockClient
 from ConfigParser import ConfigParser
 from Bio import SeqIO
 
-DefaultURL = "http://kbase.us/services/CompressionBasedDistance/"
+DefaultURL = 'http://www.kbase.us/services/cbd/'
 
 def get_url():
-    if "KB_RUNNING_IN_IRIS" not in os.environ:
-        filename = os.path.join(os.environ["HOME"], ".kbase_cbdURL")
-        if os.path.exists(filename):
-            fid = open(filename, "r")
-            currentURL = fid.readline()
-            currentURL.strip()
-            fid.close()
-        else:
-            currentURL = DefaultURL;
-    elif "KB_CBDURL" in os.environ:
-        currentURL = os.environ["KB_CBDURL"]
+    if 'KB_RUNNING_IN_IRIS' not in os.environ:
+        filename = os.path.join(os.environ['HOME'], '.kbase_cbdURL')
     else:
-        currentURL = DefaultURL
+        filename = '/.kbase_cbdURL'
+    if os.path.exists(filename):
+        fid = open(filename, 'r')
+        currentURL = fid.readline()
+        currentURL.strip()
+        fid.close()
+    else:
+        currentURL = DefaultURL;
     return currentURL
 
 def set_url(newURL):
-    if newURL ==  "default":
+    if newURL ==  'default':
         newURL = DefaultURL
-    if "KB_RUNNING_IN_IRIS" not in os.environ:
-        fid = open(os.path.join(os.environ["HOME"], ".kbase_cbdURL"), "w")
-        fid.write(newURL)
-        fid.close()
+    if 'KB_RUNNING_IN_IRIS' not in os.environ:
+        filename = os.path.join(os.environ['HOME'], '.kbase_cbdURL')
     else:
-        os.environ["KB_CBDURL"] = newURL
+        filename = '/.kbase_cbdURL'
+    fid = open(filename, 'w')
+    fid.write(newURL)
+    fid.close()
     return newURL
     
-def get_auth_token():
-    token = None
-    if "KB_RUNNING_IN_IRIS" not in os.environ:
-        filename = os.path.join(os.environ["HOME"], ".kbase_auth")
-        if os.path.exists(filename):
-            fid = open(filename, "r")
-            token = fid.readline()
-            token.strip()
-            fid.close()
-    else:
-        token = os.environ["KB_AUTH_TOKEN"]
-    return token
-
 def get_config(filename):
     # Use default config file if one is not specified.
     if filename == None:
-        filename = os.path.join(sys.environ["KB_TOP"], "deployment.cfg")
+        filename = os.path.join(sys.environ['KB_TOP'], 'deployment.cfg')
         
     # Read the config file and extract the probabilistic annotation section.
     retconfig = {}
     config = ConfigParser()
     config.read(filename)
-    for nameval in config.items("CompressionBasedDistance"):
+    for nameval in config.items('CompressionBasedDistance'):
         retconfig[nameval[0]] = nameval[1]
     return retconfig
 
