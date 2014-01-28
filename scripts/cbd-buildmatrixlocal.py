11e190ef728e3952c110d3b43ea7c2a593f6cd21
Mike Mundy <mundy.michael@mayo.edu>
2014-01-28 13:02:37 -0600

    add support to determine file format from file extension

--- scripts/cbd-buildmatrixlocal.py
+++ scripts/cbd-buildmatrixlocal.py
@@ -1,10 +1,9 @@
 import argparse
 import sys
 import os
-import time
 import traceback
 from biokbase.CompressionBasedDistance.Client import _read_inifile
-from biokbase.CompressionBasedDistance.Helpers import get_config
+from biokbase.CompressionBasedDistance.Helpers import get_config, parse_input_file
 from biokbase.CompressionBasedDistance.Worker import CompressionBasedDistance
 
 desc1 = '''
@@ -23,55 +22,33 @@ DESCRIPTION
       microbial communities.  The job started to build the distance matrix is
       run on the local system.
 
-      The inputPath positional argument is the path to a file with the list of
-      paths to the input sequence files and the groups each file belongs to.
-      Each line of the list file has two tab delimited fields: (1) path to a
-      sequence file, (2) list of groups the sequence file belongs to.  Each
-      sequence file contains the sequence reads for a microbial community.  The
-      list of groups is a semicolon delimited list of group names.  In the
-      following example, the sample1 fasta sequence file includes groups
-      patient1 and day7.
-
-          /myhome/sample1.fasta    patient1;day7
-
-      Note that the group list field is not used by cbd-buildmatrix.
-
-      The --format optional argument specifies the type of the sequence files.
-      Valid formats include 'fasta', 'fastq', 'clustal', 'embl', 'genbank',
-     'nexus, and 'seqxml'.
-
-      The --scale optional argument specifies the scale of the distance values.
-      A value of 'std' means to use the standard scale of 0 to 1.  A value of
-      'inf' means to use a scale from 0 to infinity.
-
-      A job is started to build the distance matrix and the job id is returned.
-      Use the cbd-getmatrix command to monitor the status of the job.  When the
-      job is done, the cbd-getmatrix command saves the distance matrix to a
-      file.
+      See cbd-buildmatrix for a complete description of all arguments.
 '''
 
 desc3 = '''
 EXAMPLES
-      Build a distance matrix for a set of fasta sequence files:
-      > cbd-buildmatrix mystudy.input
+      Build a distance matrix for a set of sequence files where the format is
+      determined by the file extension:
+      > cbd-buildmatrixlocal mystudy.list
 
       Build a distance matrix for a set of fastq sequence files:
-      > cbd-buildmatrix --format fastq mystudy.input
+      > cbd-buildmatrixlocal --format fastq mystudy.list
 
 SEE ALSO
       cbd-buildmatrix
       cbd-getmatrix
       cbd-filtermatrix
+      cbd-plotmatrix
 
 AUTHORS
-      Mike Mundy, Fang Yang, Nicholas Chia, Patricio Jeraldo 
+      Mike Mundy 
 '''
 
 if __name__ == "__main__":
     # Parse options.
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, prog='cbd_buildmatrix', epilog=desc3)
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, prog='cbd_buildmatrixlocal', epilog=desc3)
     parser.add_argument('inputPath', help='path to file with list of input sequence files', action='store', default=None)
-    parser.add_argument('-f', '--format', help='format of input sequence files', action='store', dest='format', default='fasta')
+    parser.add_argument('-f', '--format', help='format of input sequence files', action='store', dest='format', default=None)
     parser.add_argument('-s', '--scale', help='scale for distance matrix values', action='store', dest='scale', default='std')
     usage = parser.format_usage()
     parser.description = desc1 + '      ' + usage + desc2
@@ -80,41 +57,30 @@ if __name__ == "__main__":
     
     # Create input parameters for build_matrix() function.
     input = dict()
-    input['format'] = args.format
     input['scale'] = args.scale
     input['node_ids'] = list()
     input['file_paths'] = list()
 
-    # Create a cbd client (which must be authenticated).
+    # Get an authentication token for the current user.
     auth = _read_inifile()
-    
-    # Open the input file with the list of files.
-    try:
-        infile = open(args.inputPath, "r")
-    except IOError as e:
-        print "Error opening input list file '%s': %s" %(args.inputPath, e.strerror)
+
+    # Parse the input file with the list of sequence files.
+    (fileList, extensions, numMissingFiles) = parse_input_file(args.inputPath)
+    if numMissingFiles > 0:
         exit(1)
-  
-    # Make sure all of the files in the list of files exist. 
-    missingFiles = 0
-    filelist = []
-    for line in infile:
-        line = line.strip('\n\r')
-        if line and line[0] != '#': # Skip empty lines
-            fields = line.split('\t')
-            filename = fields[0]
-            if os.path.isfile(filename):
-                filelist.append(filename)
-            else:
-                print "'%s' does not exist" %(filename)
-                missingFiles += 1
-    infile.close()
-    if missingFiles > 0:
-        print "%d files are not accessible. Update %s with correct file names" %(missingFiles, args.inputPath)
-        exit(1)
-        
-    # For each file, upload to shock (keep track of ids).
-    for filename in filelist:
+
+    # Set the format based on the sequence file extension if the format argument was not specified.
+    if args.format is None:
+        if len(extensions) == 1:
+            input['format'] = extensions.keys()[0]
+        else:
+            print "The format of the sequence files could not be determined.  Set the format with the --format argument."
+            exit(1)
+    else:
+        input['format'] = args.format
+
+    # For each file, add it to the list.
+    for filename in fileList:
         input['file_paths'].append(filename)
         
     # Submit a job to build the distance matrix.
